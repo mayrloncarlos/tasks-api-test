54aec16b8270cf724a75c01ac705bb83ba43649d
Mayrlon Carlos <mayrloncarlos@gmail.com>
2020-03-28 05:49:15 -0300

    First commit

---
+++ src/test/java/br/com/mayrloncarlos/tasks/apitest/APITest.java
@@ -0,0 +1,54 @@
+package br.com.mayrloncarlos.tasks.apitest;
+
+import static io.restassured.RestAssured.given;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend/";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+		;
+	}
+	
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		given()
+			.body("{\"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		given()
+			.body("{\"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+
+}
